c1611ab66f47eed60d04eea1da59c146b51d8c70
Paul B <paul.belfrage@gmail.com>
2020-03-25 19:07:39 +0100

    player script
    player script

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public string myName = "Adam";
+    public int myAge = 22;
+    public float mySpeed = 10.2f;
+    public int health = 100;
+    public int score = 120;
+    public int hasAllKeys;
+    public int ammoCount;
+
+    void Start()
+    {
+        Debug.Log("My name is" + myName + " and is am " + myAge + " years old");
+        Debug.Log("Speed: " + mySpeed);
+        Debug.Log("Health: " + health);
+        Debug.Log("Score: " + score);
+        Debug.Log("Has All Keys: " + hasAllKeys);
+        Debug.Log("Ammo Count " + ammoCount);
+
+    }
+
+
+}
