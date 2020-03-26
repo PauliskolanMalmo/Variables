6c86d573780e087f51be25e818fc9b07b6d883da
Paul B <paul.belfrage@gmail.com>
2020-03-26 08:39:49 +0100

    Quiz
    Quiz

---
+++ Assets/QuizCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCalculator : MonoBehaviour
+{
+
+    public float q1, q2, q3, q4, q5;
+    void Start()
+    {
+        q1 = Random.Range(0f, 100f);
+        q2 = Random.Range(0f, 100f);
+        q3 = Random.Range(0f, 100f);
+        q4 = Random.Range(0f, 100f);
+        q5 = Random.Range(0f, 100f);
+
+        float average = (q1 + q2 + q3 + q4 + q5) / 5;
+
+        Debug.Log("Average: " + average);
+        Debug.Log("Average: " + Mathf.Round(average * 100f) / 100f);
+
+        //Alt. way - https://answers.unity.com/questions/535424/round-float-with-2-decimal.html
+        Debug.Log("Average: " + System.Math.Round(average, 2));
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/TipCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour
+{
+    public int bill = 40;
+    public float tip = 20.0f;
+    public float totalAmount;
+
+    void Start()
+    {
+        float tipAmount = bill * (tip / 100);
+        totalAmount = bill + tipAmount;
+
+        Debug.Log("Your bill is: " + bill + " and your amount is: " + tipAmount + " So you owe: " + totalAmount);
+    }
+
+
+}
